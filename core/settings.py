05f79952ebbd14e4bdcd047e79262941ad57ee16
mithun8441 <mahmudulhasan3527@gmail.com>
2026-07-14 00:44:19 +0600

    Add CSRF_TRUSTED_ORIGINS for localhost

--- core/settings.py
+++ core/settings.py
@@ -149,6 +149,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Dravon Server API',
     'DESCRIPTION': 'API documentation for Dravon Server',
